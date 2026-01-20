dfea49edcb201166dec757deb4255f722875e91f
nico386d <nico386d@gmail.com>
2026-01-20 11:58:49 +0100

    ONNX export
    Introduces onnix  to give faster and more scalable inference than running pytorch directly inside fastAPI

---
+++ src/ct_scan_mlops/onnx_export.py
@@ -0,0 +1,52 @@
+import torch
+from omegaconf import OmegaConf
+from pathlib import Path
+
+from ct_scan_mlops.model import build_model
+
+DEVICE = torch.device("cpu")  # ONNX export should be CPU
+
+
+def main():
+    # 🔧 CHANGE THIS if needed
+    run_dir = Path("outputs/2026-01-15/11-57-23")
+
+    config_path = run_dir / ".hydra" / "config.yaml"
+    model_path = run_dir / "model.pt"
+    onnx_path = run_dir / "model.onnx"
+
+    if not config_path.exists():
+        raise FileNotFoundError(f"Config not found: {config_path}")
+
+    if not model_path.exists():
+        raise FileNotFoundError(f"Model not found: {model_path}")
+
+    # Load Hydra config
+    cfg = OmegaConf.load(config_path)
+
+    # Build model
+    model = build_model(cfg)
+    model.load_state_dict(torch.load(model_path, map_location="cpu"))
+    model.eval()
+    model.to(DEVICE)
+
+    # Dummy input (must match training input)
+    image_size = cfg.data.image_size
+    dummy_input = torch.randn(1, 3, image_size, image_size)
+
+    # Export
+    torch.onnx.export(
+        model,
+        dummy_input,
+        onnx_path,
+        input_names=["input"],
+        output_names=["output"],
+        opset_version=18,
+    )
+
+
+    print(f"ONNX model saved to: {onnx_path}")
+
+
+if __name__ == "__main__":
+    main()
